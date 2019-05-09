8ddc600207e706ae41ea59872bf9097d22229180
Kevin Rotairo <kevin@centricitgroup.com>
2019-05-09 18:18:50 +0800

    added models.py

--- app/config.py
+++ app/config.py
@@ -1,3 +1,6 @@
 #Config variables for Flask App
+import os
 class Configuration(object):
+    APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__))
     DEBUG = True
+    SQLALCEHMY_DATABASE_URI = 'sqlite:///%s/blog.db' % APPLICATION_DIR
