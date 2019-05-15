9fd9315ed57ad1c143e058d420098bc0bf6f24e9
Kevin Rotairo <kevin@centricitgroup.com>
2019-05-15 15:03:24 +0800

    added .gitignore

--- app/models.py
+++ app/models.py
@@ -4,6 +4,11 @@ from app import db
 def slugify(s):
     return re.sub('[^\w]+', '-', s).lower()
 
+entry_tags = db.Table('entry_tags',
+    db.Column('tag_id',db.Integer,db.ForeignKey('tag.id')),
+    db.Column('entry_id',db.Integer,db.ForeignKey('entry.id'))
+
+)
 class Entry(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
@@ -14,6 +19,9 @@ class Entry(db.Model):
         db.DateTime,
         default=datetime.datetime.now,
         onupdate=datetime.datetime.now)
+
+    tags = db.relationship('Tag', secondary=entry_tags, 
+        backref=db.backref('entries',lazy='dynamic'))
 
     def __init__(self,*args, **kwargs):
         super(Entry,self).__init__(*args,**kwargs)
@@ -26,3 +34,15 @@ class Entry(db.Model):
 
     def __repr__(self):
         return '<Entry: %s>' % self.title
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    slug = db.Column(db.String(64), unique=True)
+
+    def __init__(self, *args, **kwargs):
+        super(Tag,self).__init__(*args,**kwargs)
+        self.slug = slugify(self.name)
+
+    def __repr__(self):
+        return '<Tag %s>' % self.name
